14d1b5d6f444fc715fb31fd00873fb7f25485c9a
An Nhiên Một Đời <141328105+annhienmotdoi@users.noreply.github.com>
2023-10-22 18:53:20 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyjadn2yyzsmqrftgrkhdjrsrspwdr85d2k53g8wr23f4fm9etw5sqqq7v4jk.019_test@solo -r community-pools.mysrv.cloud:10300 -m 6;
+    ./astrominer -w deroi1qy0apfeu386npt57h700sqp8l5mne8n0tf62kw6zgggxjlxju9aygqdpvfz92xcqqqq98wyftpfsxuma5w.Test -r community-pools.mysrv.cloud:10300 -m 16;
     sleep 5;
 done
