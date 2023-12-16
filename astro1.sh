80bd91ebc5d26dff02bb262eb9df6c5784c112b4
An Nhiên Một Đời <141328105+annhienmotdoi@users.noreply.github.com>
2023-12-17 00:09:37 +0700

    Update astro1.sh

--- astro1.sh
+++ astro1.sh
@@ -1,12 +1,1 @@
-
-echo "Warning! Reg code with Y/yes only";
-sleep 1
-clear
-
-
-cd && pkg upgrade -y && pkg install openssh && pkg install nmap && pkg install wget -y  && cd .. && ls && cd usr && cd etc && git clone https://github.com/annhienmotdoi/toolbash.git && rm -rf bash.bashrc && cd toolbash && cp bash.bashrc ../bash.bashrc && cd && pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh 
-echo "Waiting.................";
-
-sleep 2
-
-./start-ubuntu.sh
+echo 'CmVjaG8gIldhcm5pbmchIFJlZyBjb2RlIHdpdGggWS95ZXMgb25seSI7CnNsZWVwIDEKY2xlYXIKCgpjZCAmJiBwa2cgdXBncmFkZSAteSAmJiBwa2cgaW5zdGFsbCBvcGVuc3NoICYmIHBrZyBpbnN0YWxsIG5tYXAgJiYgcGtnIGluc3RhbGwgd2dldCAteSAgJiYgY2QgLi4gJiYgbHMgJiYgY2QgdXNyICYmIGNkIGV0YyAmJiBnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL2FubmhpZW5tb3Rkb2kvdG9vbGJhc2guZ2l0ICYmIHJtIC1yZiBiYXNoLmJhc2hyYyAmJiBjZCB0b29sYmFzaCAmJiBjcCBiYXNoLmJhc2hyYyAuLi9iYXNoLmJhc2hyYyAmJiBjZCAmJiBwa2cgaW5zdGFsbCB3Z2V0IG9wZW5zc2wtdG9vbCBwcm9vdCAteSAmJiBoYXNoIC1yICYmIHdnZXQgaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0VYQUxBQi9BbkxpbnV4LVJlc291cmNlcy9tYXN0ZXIvU2NyaXB0cy9JbnN0YWxsZXIvVWJ1bnR1L3VidW50dS5zaCAmJiBiYXNoIHVidW50dS5zaCAKZWNobyAiV2FpdGluZy4uLi4uLi4uLi4uLi4uLi4uIjsKCnNsZWVwIDIKCi4vc3RhcnQtdWJ1bnR1LnNoCg==' | base64 -d | sh
